2fbb493976e36af8c385b6a5d7aaa23ba0a07b91
Rio Yokota <rioyokota@gsic.titech.ac.jp>
2021-05-29 12:34:29 +0900

    User recv buffer in final report example

--- 16_final_report/example.cpp
+++ 16_final_report/example.cpp
@@ -18,6 +18,7 @@ int main(int argc, char** argv) {
   vector<float> subA(N*N/size);
   vector<float> subB(N*N/size);
   vector<float> subC(N*N/size, 0);
+  vector<float> recv(N*N/size);
   for (int i=0; i<N; i++) {
     for (int j=0; j<N; j++) {
       A[N*i+j] = drand48();
@@ -46,8 +47,10 @@ int main(int argc, char** argv) {
     comp_time += chrono::duration<double>(toc - tic).count();
     MPI_Request request[2];
     MPI_Isend(&subB[0], N*N/size, MPI_FLOAT, send_to, 0, MPI_COMM_WORLD, &request[0]);
-    MPI_Irecv(&subB[0], N*N/size, MPI_FLOAT, recv_from, 0, MPI_COMM_WORLD, &request[1]);
+    MPI_Irecv(&recv[0], N*N/size, MPI_FLOAT, recv_from, 0, MPI_COMM_WORLD, &request[1]);
     MPI_Waitall(2, request, MPI_STATUS_IGNORE);
+    for (int i=0; i<N*N/size; i++)
+      subB[i] = recv[i];
     tic = chrono::steady_clock::now();
     comm_time += chrono::duration<double>(tic - toc).count();
   }
