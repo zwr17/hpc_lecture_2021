20e3c3e783f7faeb00f0cad5b184a09e451da8af
wr z <wrz@wrnoMacBook-Pro.local>
2021-05-31 16:35:52 +0900

    update mpi part

--- 16_final_report/mpi_open_SIMD.cpp
+++ 16_final_report/mpi_open_SIMD.cpp
@@ -16,7 +16,7 @@ int main(int argc, char **argv) {
   MPI_Comm_size(MPI_COMM_WORLD, &size);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-  const int N = 256;
+  const int N = 4096;
   vector<float> A(N * N);
   vector<float> B(N * N);
   vector<float> C(N * N, 0);
@@ -24,6 +24,7 @@ int main(int argc, char **argv) {
   vector<float> subB(N * N / size);
   vector<float> subC(N * N / size, 0);
   vector<float> recv(N * N / size);
+
   for (int i=0; i<N; i++) {
     for (int j=0; j<N; j++) {
       A[N * i + j] = drand48();
@@ -38,39 +39,41 @@ int main(int argc, char **argv) {
   for (int i = 0; i < N; i++)
       for (int j = 0; j < N / size; j++)
         subB[N / size * i + j] = B[N * i + j + offset];
+
   int recv_from = (rank + 1) % size;
   int send_to = (rank - 1 + size) % size;  
   
   const int m = N, n = N, k = N;
-  const int kc = 128;
-  const int nc = 16;
-  const int mc = 64;
-  const int nr = 16;
-  const int mr = 8;
+  const int kc = 512;
+  const int nc = 64;
+  const int mc = 256;
+  const int nr = 64;
+  const int mr = 32;
 
   double comp_time = 0, comm_time = 0;
+
   for (int irank = 0; irank < size; irank++){
     auto tic = chrono::steady_clock::now();
     offset = N / size * ((rank + irank) % size);
-
-  for (int jc=0; jc<N/size; jc+=nc) {
-    for (int pc=0; pc<k; pc+=kc) {
-      float Bc[kc*nc];
-      for (int p=0; p<kc; p++) {
-        for (int j=0; j<nc; j++) {
-          Bc[p*nc+j] = subB[(N/size)*(p+pc)+j+jc];//???
-        }
-      }
-      for (int ic=0; ic<N/size; ic+=mc) {
-        float Ac[mc*kc],Cc[mc*nc];
-        for (int i=0; i<mc; i++) {
-          for (int p=0; p<kc; p++) {
-            Ac[i*kc+p] = subA[(N/size)*(i+ic)+p+pc];
-          }
+    #pragma omp parallel for collapse(2)
+    for (int jc=0; jc<N/size; jc+=nc) {
+      for (int pc=0; pc<k; pc+=kc) {
+        float Bc[kc*nc];
+        for (int p=0; p<kc; p++) {
           for (int j=0; j<nc; j++) {
-            Cc[i*nc+j] = 0;
+            Bc[p*nc+j] = subB[(N/size)*(p+pc)+j+jc];//???
           }
         }
+        for (int ic=0; ic<N/size; ic+=mc) {
+          float Ac[mc*kc],Cc[mc*nc];
+          for (int i=0; i<mc; i++) {
+            for (int p=0; p<kc; p++) {
+              Ac[i*kc+p] = subA[N*(i+ic)+p+pc];
+            }
+            for (int j=0; j<nc; j++) {
+              Cc[i*nc+j] = 0;
+            }
+          }
         for (int jr=0; jr<nc; jr+=nr) {
           for (int ir=0; ir<mc; ir+=mr) {
             for (int kr=0; kr<kc; kr++) {
@@ -88,6 +91,7 @@ int main(int argc, char **argv) {
         }
         for (int i=0; i<mc; i++) {
           for (int j=0; j<nc; j++) {
+            #pragma omp atomic update
             subC[N*(i+ic)+j+jc+offset] += Cc[i*nc+j];
           }
         }
@@ -111,7 +115,7 @@ int main(int argc, char **argv) {
 MPI_Allgather(&subC[0], N * N / size, MPI_FLOAT, &C[0], N * N / size, MPI_FLOAT, MPI_COMM_WORLD);
   
 //printf("N=%d: %lf s (%lf GFlops)\n",N,time,2.*N*N*N/time/1e9);
-
+#pragma omp parallel for
   for (int i=0; i<N; i++)
     for (int k=0; k<N; k++)
       for (int j=0; j<N; j++)
